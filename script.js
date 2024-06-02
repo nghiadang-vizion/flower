6c22c5d3a38a7ddb98c6f7f2897f331a0567758a
nghiadang-vizion <108381566+nghiadang-vizion@users.noreply.github.com>
2024-06-02 15:06:12 +0700

    3rd commit

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 onload = () => {
   document.body.classList.remove("container");
 };
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", myFunction);
+
+function myFunction() {
+  const music = document.querySelector(".music");
+  music.play();
+
+  const animateBackGround = document.querySelector(".animate_container");
+  animateBackGround.classList.toggle("appear");
+
+  const lyrics = document.querySelector(".lyrics");
+  lyrics.style.display = "block";
+
+  var svg = document.querySelectorAll(".draw");
+  svg.forEach((i) => i.classList.toggle("reverse"));
+}
